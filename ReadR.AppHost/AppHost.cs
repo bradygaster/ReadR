b9d923f4ac17cc0de755981315b85bb4a629a7a9
bradygaster <bradyg@microsoft.com>
2025-08-02 16:19:49 -0700

    added storage

--- ReadR.AppHost/AppHost.cs
+++ ReadR.AppHost/AppHost.cs
@@ -1,9 +1,26 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-// front end project
-var frontend = builder.AddProject<Projects.ReadR_Frontend>("frontend");
+// azure storage
+var storage = builder.AddAzureStorage("readrstorage")
+                     .RunAsEmulator();
+
+var readrblobs = storage.AddBlobs("readrblobs");
+var readrqueues = storage.AddQueues("readrqueues");
 
 // functions project
-var functions = builder.AddAzureFunctionsProject<Projects.ReadR_Serverless>("functions");
+var functions = builder.AddAzureFunctionsProject<Projects.ReadR_Serverless>("functions")
+                       .WithHostStorage(storage)
+                       .WaitFor(readrblobs)
+                       .WaitFor(readrqueues)
+                       .WithReference(readrblobs)
+                       .WithReference(readrqueues);
+
+// front end project
+var frontend = builder.AddProject<Projects.ReadR_Frontend>("frontend")
+                      .WaitFor(readrqueues)
+                      .WaitFor(readrblobs)
+                      .WaitFor(functions)
+                      .WithReference(readrqueues)
+                      .WithReference(readrblobs);
 
 builder.Build().Run();
