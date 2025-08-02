7f013416eff7a964e3e6bf7e6092d1e7928eda36
bradygaster <bradyg@microsoft.com>
2025-08-02 16:32:41 -0700

    publishing all as container apps, with proper storage roles set up

--- ReadR.AppHost/AppHost.cs
+++ ReadR.AppHost/AppHost.cs
@@ -1,4 +1,9 @@
+using Azure.Provisioning.Storage;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+// Add the Azure Container App environment
+builder.AddAzureContainerAppEnvironment("readracaenv");
 
 // azure storage
 var storage = builder.AddAzureStorage("readrstorage")
@@ -13,7 +18,15 @@ var functions = builder.AddAzureFunctionsProject<Projects.ReadR_Serverless>("fun
                        .WaitFor(readrblobs)
                        .WaitFor(readrqueues)
                        .WithReference(readrblobs)
-                       .WithReference(readrqueues);
+                       .WithReference(readrqueues)
+                       .WithRoleAssignments(storage,
+                            StorageBuiltInRole.StorageBlobDataOwner,
+                            StorageBuiltInRole.StorageQueueDataContributor,
+                            StorageBuiltInRole.StorageTableDataContributor
+                        )
+                       .PublishAsAzureContainerApp((aspireResource, containerApp) =>
+                       {
+                       });
 
 // front end project
 var frontend = builder.AddProject<Projects.ReadR_Frontend>("frontend")
@@ -21,6 +34,14 @@ var frontend = builder.AddProject<Projects.ReadR_Frontend>("frontend")
                       .WaitFor(readrblobs)
                       .WaitFor(functions)
                       .WithReference(readrqueues)
-                      .WithReference(readrblobs);
+                      .WithReference(readrblobs)
+                      .WithRoleAssignments(storage,
+                           StorageBuiltInRole.StorageBlobDataOwner,
+                           StorageBuiltInRole.StorageQueueDataContributor,
+                           StorageBuiltInRole.StorageTableDataContributor
+                      )
+                      .PublishAsAzureContainerApp((aspireResource, containerApp) =>
+                      {
+                      });
 
 builder.Build().Run();
