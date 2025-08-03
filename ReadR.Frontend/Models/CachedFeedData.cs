0407b74f855c66f9caf3a0e989d4d85a78c09139
bradygaster <bradyg@microsoft.com>
2025-08-03 13:48:10 -0700

    deleted project and workflow files to reset repo's main

--- ReadR.Frontend/Models/CachedFeedData.cs
+++
@@ -1,9 +0,0 @@
-namespace ReadR.Frontend.Models;
-
-public class CachedFeedData
-{
-    public List<FeedEntry> AllEntries { get; set; } = new();
-    public CategorizedFeeds WorkingFeeds { get; set; } = new();
-    public Dictionary<string, FeedMetadata> FeedMetadata { get; set; } = new();
-    public DateTime LastUpdated { get; set; }
-}
