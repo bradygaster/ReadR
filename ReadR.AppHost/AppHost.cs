6205feccbf8b3ae0031203eb74cc9f471b0df2c4
bradygaster <bradyg@microsoft.com>
2025-08-03 11:25:57 -0700

    backed out all the integrations to rely solely on the connected services-configured connections.

--- ReadR.AppHost/AppHost.cs
+++ ReadR.AppHost/AppHost.cs
@@ -1,32 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-// azure storage
-var storage = builder.AddAzureStorage("readrstorage")
-                     .RunAsEmulator()
-                     ;
-
-var readrblobs = storage.AddBlobs("readrblobs");
-var readrqueues = storage.AddQueues("readrqueues");
-
 // functions project
-var functions = builder.AddAzureFunctionsProject<Projects.ReadR_Serverless>("functions")
-                       .WithHostStorage(storage)
-                       .WaitFor(readrblobs)
-                       .WaitFor(readrqueues)
-                       .WithReference(readrblobs)
-                       .WithReference(readrqueues)
-                       .WithEnvironment("readrblobs__blobServiceUri", readrblobs)
-                       .WithEnvironment("readrqueues__queueServiceUri", readrqueues);
+var functions = builder.AddAzureFunctionsProject<Projects.ReadR_Serverless>("functions");
 
 // front end project
 var frontend = builder.AddProject<Projects.ReadR_Frontend>("frontend")
-                      .WithExternalHttpEndpoints()
-                      .WaitFor(readrqueues)
-                      .WaitFor(readrblobs)
-                      .WaitFor(functions)
-                      .WithReference(readrqueues)
-                      .WithReference(readrblobs)
-                      .WithEnvironment("readrblobs__blobServiceUri", readrblobs)
-                      .WithEnvironment("readrqueues__queueServiceUri", readrqueues);
+                      .WithExternalHttpEndpoints();
 
 builder.Build().Run();
